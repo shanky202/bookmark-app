9730ad42ccc789976968a91be8a8aac8d6a48c2e
shanky202 <shankypachore@gmail.com>
2020-08-25 18:06:50 +0000

    modified index.html, style.css & script.js to add functionality

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("testing");
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+};
+
+// Modal Event Listener
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click',(e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(name, url) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!name || !url) {
+        alert('Please submit values for both fields');
+        return false;
+    }
+    if (!url.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    return true;
+
+}
+// Handle Data from Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark)
